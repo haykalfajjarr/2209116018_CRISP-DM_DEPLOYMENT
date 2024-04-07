995663d35eb365bb033650a64b0e1826b8fe2250
haykalfajjarr <162568397+haykalfajjarr@users.noreply.github.com>
2024-04-07 11:41:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import altair as alt
 import matplotlib.pyplot as plt
 from function import *
-
+from streamlit_option_menu import *
 df = pd.read_csv('insurance.csv')
 
 with st.sidebar :
